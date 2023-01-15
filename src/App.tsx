25596a552d7bd09e2b928410f9ea86e8ab04ade4
colin_greens <colin.j.benson@gmail.com>
2023-01-14 19:41:12 -0800

    added font, routing and basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import User from "./assets/user/pages/User";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<User />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/assets/user/pages/User.tsx
@@ -0,0 +1,5 @@
+import "./User.css";
+
+export default function User() {
+  return <h1>Landing Page!</h1>;
+}
