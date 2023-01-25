7c79c84addad481971e5ab8c81d71955ccb27ab1
colin_greens <colin.j.benson@gmail.com>
2023-01-24 19:29:06 -0800

    adjusted character select box

--- src/user/pages/CharacterSelect.jsx
+++ src/user/pages/CharacterSelect.jsx
@@ -2,13 +2,12 @@ import CharacterSelectList from "../components/CharacterSelectList";
 import classes from "./CharacterSelect.module.css";
 
 export default function CharacterSelect() {
-  const name = "panda";
   return (
     <div className={classes["background-image"]}>
       <div className={classes.title}>
         <h2>Choose your Character</h2>
       </div>
-      <div className={classes.container}>
+      <div className={classes.characters}>
         <CharacterSelectList
           characterItems={[
             { name: "fire-mage" },
