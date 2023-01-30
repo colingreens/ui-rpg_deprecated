3d9ed74609576d3309f080cb36dd4d14ff5e4a34
colin_greens <colin.j.benson@gmail.com>
2023-01-30 00:05:39 -0800

    wip card slider

--- src/user/pages/Combat.jsx
+++ src/user/pages/Combat.jsx
@@ -3,13 +3,15 @@ import classes from "./Combat.module.css";
 export default function Combat(props) {
   return (
     <div>
-      <div className={classes.background}>Background</div>
-      <div className={classes.stage}>
-        <span className={classes.player}></span>
-        <span className={classes.enemy}></span>
-      </div>
-      <div className={classes.cards}>
-        <div>Combat Cards</div>
+      <div className={classes.background}>
+        <h2 className="center">Fight</h2>
+        <div className={classes.stage}>
+          <span className={classes.player}></span>
+          <span className={classes.enemy}></span>
+        </div>
+        <div className={classes.cards}>
+          <div>Combat Cards</div>
+        </div>
       </div>
     </div>
   );
